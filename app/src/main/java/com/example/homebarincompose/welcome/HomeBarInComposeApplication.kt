749eb7e497281c486acf75b386f5961e7151411e
EdyWDev <edyta.wysocka.1994@gmail.com>
2024-01-30 11:20:38 +0100

    adding @Hilt

---
+++ app/src/main/java/com/example/homebarincompose/welcome/HomeBarInComposeApplication.kt
@@ -0,0 +1,8 @@
+package com.example.homebarincompose.welcome
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HomeBarInComposeApplication: Application() {
+}
